66852f039226fbb60624462d45ba0f0cb302c9b7
原　崚河 <k022g0002@g.neec.ac.jp>
2024-01-30 02:26:08 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+template <typename T>
+class MinFinder {
+public:
+
+    T Min(T a, T b) {
+        return (a < b) ? a : b;
+    }
+};
+
+int main() {
+    // int型のインスタンス
+    MinFinder<int> intMinFinder;
+    int intResult = intMinFinder.Min(5, 10);
+    printf("Min:5 or 10 <int>: %d\n", intResult);
+
+    // float型のインスタンス
+    MinFinder<float> floatMinFinder;
+    float floatResult = floatMinFinder.Min(3.14f, 2.71f);
+    printf("Min:3.14 or 2.71 <float>: %f\n", floatResult);
+
+    // double型のインスタンス
+    MinFinder<double> doubleMinFinder;
+    double doubleResult = doubleMinFinder.Min(10.5, 8.9);
+    printf("Min:10.5 or 8.9 <double>: %lf\n", doubleResult);
+
+    return 0;
+}
