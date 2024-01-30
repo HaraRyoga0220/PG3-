eca5ebb22f7a8900909c52e652fec9018aac8b28
原　崚河 <k022g0002@g.neec.ac.jp>
2024-01-30 11:48:13 +0900

    2-2

--- main.cpp
+++ main.cpp
@@ -1,43 +1,40 @@
 #include <stdio.h>
 
+
 template <typename T>
 class MinFinder {
 public:
+    // 2つの引数を比べ小さい値を返す
     T Min(T a, T b) {
         return (a < b) ? a : b;
     }
 };
 
 int main() {
-    // int型のインスタンス
-    MinFinder<int> intMinFinder;
-    int intResult = intMinFinder.Min(5, 10);
-    printf("Min: 5 or 10 <int>: %d\n", intResult);
-
-    // float型のインスタンス
+    // float型の組み合わせ
     MinFinder<float> floatMinFinder;
     float floatResult = floatMinFinder.Min(3.14f, 2.71f);
     printf("Min: 3.14 or 2.71 <float>: %f\n", floatResult);
 
-    // double型のインスタンス
+    // double型の組み合わせ
     MinFinder<double> doubleMinFinder;
     double doubleResult = doubleMinFinder.Min(10.5, 8.9);
     printf("Min: 10.5 or 8.9 <double>: %lf\n", doubleResult);
 
-    // int型のインスタンス (再度)
-    MinFinder<int> intMinFinder2;
-    int intResult2 = intMinFinder2.Min(5, 10);
-    printf("Min: 5 or 10 <int>: %d\n", intResult2);
+    // int型とdouble型
+    MinFinder<int> intDoubleMinFinder;
+    int intDoubleResult = intDoubleMinFinder.Min(5, 8.9);
+    printf("Min: 5 or 8.9 <int>: %d\n", intDoubleResult);
 
-    // float型のインスタンス (異なる値)
-    MinFinder<float> floatMinFinder2;
-    float floatResult2 = floatMinFinder2.Min(5, 10);
-    printf("Min: 5 or 10 <int>: %f\n", floatResult2);
+    // int型とfloat型
+    MinFinder<int> intFloatMinFinder;
+    int intFloatResult = intFloatMinFinder.Min(5, 2.71f);
+    printf("Min: 5 or 2.71 <int>: %d\n", intFloatResult);
 
-    // double型のインスタンス (異なる値)
-    MinFinder<double> doubleMinFinder2;
-    double doubleResult2 = doubleMinFinder2.Min(5, 10);
-    printf("Min: 5 or 10 <int>: %lf\n", doubleResult2);
+    // float型とdouble型
+    MinFinder<float> floatDoubleMinFinder;
+    float floatDoubleResult = floatDoubleMinFinder.Min(3.14f, 8.9);
+    printf("Min: 3.14 or 8.9 <float>: %f\n", floatDoubleResult);
 
     return 0;
 }
