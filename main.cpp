ab0c1f24f2f0f220e963b20bb5ab1cdb1579a551
原　崚河 <k022g0002@g.neec.ac.jp>
2024-01-30 11:14:31 +0900

    2-3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
-#include "Enemy.h"
+#include <iostream>
+
+// 動物クラス
+class Animal {
+public:
+ 
+    virtual void MakeSound() const {
+        std::cout << "Some generic animal sound" << std::endl;
+    }
+};
+
+// 犬クラス
+class Dog : public Animal {
+public:
+    void MakeSound() const override {
+        std::cout << "One" << std::endl;
+    }
+};
+
+// 猫クラス
+class Cat : public Animal {
+public:
+ 
+    void MakeSound() const override {
+        std::cout << "Nya!" << std::endl;
+    }
+};
 
 int main() {
-    Enemy enemy;
+    Animal* animal1 = new Dog();
+    Animal* animal2 = new Cat();
 
-    // シーンの更新を数回繰り返す例
-    for (int i = 0; i < 3; ++i) {
-        enemy.Update();
-    }
+  
+    animal1->MakeSound(); 
+    animal2->MakeSound(); 
+
+    delete animal1;
+    delete animal2;
 
     return 0;
 }
