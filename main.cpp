e03662bf83c33d3cb79a20fa9ea393a5e3aa5a39
原　崚河 <k022g0002@g.neec.ac.jp>
2024-02-08 03:15:28 +0900

    6-2完了

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ void Thread3() {
 int main() {
 	std::thread th1(Thread1);
 	th1.join();
-	std::thread th2(Thread2);
+	std::thread th2(Thread2);;
 	th2.join();
 	std::thread th3(Thread3);
 	th3.join();
