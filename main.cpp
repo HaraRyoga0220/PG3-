b79aabcc601da34bd05734fc7897e9379aa89ffb
原　崚河 <k022g0002@g.neec.ac.jp>
2024-02-12 14:57:57 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+#include <list>
+
+int main(void) {
+	SetConsoleOutputCP(65001);
+
+	std::list<const char*> yamanoteStationName = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho",
+	};
+
+	std::cout << "1970”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+	
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
+	}
+
+	std::cout << "\n" << std::endl;
+
+	auto findStationNameIterator = std::find(yamanoteStationName.begin(), yamanoteStationName.end(), "Nippori");
+	findStationNameIterator++;
+	yamanoteStationName.insert(findStationNameIterator, "Nishi-Nippori");
+
+	std::cout << "2019”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+	
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
+	}
+
+	std::cout << "\n" << std::endl;
+
+	findStationNameIterator = std::find(yamanoteStationName.begin(), yamanoteStationName.end(), "Shinagawa");
+	findStationNameIterator++;
+	findStationNameIterator = yamanoteStationName.insert(findStationNameIterator, "Takanawa Gateway");
+
+	std::cout << "2022”N‚ÌŽRŽèü‰wˆê——" << std::endl;
+
+	for (auto& name : yamanoteStationName) {
+		std::cout << name << std::endl;
+	}
+
+	return 0;
+}
